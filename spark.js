2e9ae3916c20f8a409387b86699022c6d1710f25
Fasweeh <104542292+inrlpub@users.noreply.github.com>
2022-04-28 11:48:54 +0530

    Update spark.js

--- spark.js
+++ spark.js
@@ -14,9 +14,9 @@ module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
  BUTTON: process.env.BUTTON_TEXT === undefined ? false : process.env.BUTTON_TEXT,
  WEBPREVIEW: process.env.FAKE_PREVIEW === undefined ? false : process.env.FAKE_PREVIEW,
- PHONE: process.env.NUMBER === undefined ? '919526808481' : process.env.NUMBER,   
+ PHONE: process.env.NUMBER === undefined ? '917025099161' : process.env.NUMBER,   
  OA_NAME: process.env.DEPLOYER === undefined ? 'undefined' : process.env.DEPLOYER,    
  THUMBNAIL: process.env.THUMBNAIL_URL === undefined ? 'https://i.imgur.com/xsKCqwZ.jpeg' : process.env.THUMBNAIL_URL,
- VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[Shadow Bot]\nCoded by @SPARK-SHADOW' : process.env.VERIFICATION_CAPTION,
- SHADOWPW: process.env._SHADOW_BOT_ACSESS_PASSWORD === undefined ? false : process.env._SHADOW_BOT_ACSESS_PASSWORD,
+ VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[inrlbot]\nCoded by @Sinrlpub' : process.env.VERIFICATION_CAPTION,
+ INRLPW: process.env._INRL_BOT_ACSESS_PASSWORD === undefined ? false : process.env._INRL_BOT_ACSESS_PASSWORD,
 };
